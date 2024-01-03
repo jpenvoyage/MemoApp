daa26bc7cc8d6dacf2be8ddf7fafb68dcfd46254
joaobug <joaoenfrance@gmail.com>
2024-01-03 16:03:25 -0300

    Initial Commit

---
+++ MemorizeApp/MemorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeAppApp.swift
+//  MemorizeApp
+//
+//  Created by Joao Leal on 03/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
